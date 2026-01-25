c4930a77895f8f50d985695c09bc06c73fa0a9b4
Joosetha D <joosedhivya2006@gmail.com>
2026-01-25 14:26:39 +0530

    Add files via upload

---
+++ 43.c
@@ -0,0 +1,22 @@
+//43.Greater than or equal to  or Less than equal to 
+
+//>=
+#include<stdio.h>
+int main(){
+	int a,b;
+	Scanf("%d",&a);
+	scanf("%d",&b);
+	printf("%d",a>=b);
+	return 0;
+}
+
+
+//<=
+#include<stdio.h>
+int main(){
+	int a,b;
+	Scanf("%d",&a);
+	scanf("%d",&b);
+	printf("%d",a<=b);
+	return 0;
+}
