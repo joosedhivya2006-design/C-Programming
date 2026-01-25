32bfaedaa90384311e54654d700a62c24c17b090
Joosetha D <joosedhivya2006@gmail.com>
2026-01-25 14:13:42 +0530

    Add files via upload

---
+++ 13.c
@@ -0,0 +1,12 @@
+//13.change variable value and print updated value
+
+#include <stdio.h>
+int main(){
+	int value;
+	scanf("%d",&value);
+	int temp=value;
+	scanf("%d",&value);
+	printf("%d",value);
+
+	return 0;
+}
