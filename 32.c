867a33f31b18e8466c7b52a8ddbc2b260cc4fa0a
Joosetha D <joosedhivya2006@gmail.com>
2026-01-25 14:22:39 +0530

    Add files via upload

---
+++ 32.c
@@ -0,0 +1,11 @@
+//32.Print the quotient and remainder of two integers
+
+#include <stdio.h>
+int main(){
+	int a,b;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	printf("Quotient = %d ",a/b);
+	printf("Remainder = %d",a%b);
+	return 0;
+}
