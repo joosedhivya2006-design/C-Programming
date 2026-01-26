eb4ba62b15ae8514762983cfab804014302ea009
Joosetha D <joosedhivya2006@gmail.com>
2026-01-26 08:00:02 +0530

    Add files via upload

---
+++ 54.c
@@ -0,0 +1,11 @@
+//54.Check odd or even
+
+#include <stdio.h>
+int main(){
+	int number;
+	scanf("%d",&number);
+	if(number%2==0)
+	{ printf("Even Number:);}
+	else{printf("Odd Number");}
+	return 0;
+}
