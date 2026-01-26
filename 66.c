37f70dd5b6fc939afca8597f68352dc666ef688a
Joosetha D <joosedhivya2006@gmail.com>
2026-01-26 17:30:50 +0530

    Add files via upload

---
+++ 66.c
@@ -0,0 +1,13 @@
+//66.Divisible by 11
+#include <stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    if(a%11==0){
+        printf("Divisible by 11");
+    }
+    else{
+        printf("Not Divisible by 11");
+    }
+    return 0;
+}
