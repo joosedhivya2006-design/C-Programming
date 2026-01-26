df91df45cce9f7538790c17ceb96be8fee3dac14
Joosetha D <joosedhivya2006@gmail.com>
2026-01-26 08:00:35 +0530

    Add files via upload

---
+++ 56.c
@@ -0,0 +1,12 @@
+//56.Find Greater of Two numbers
+
+#include <stdio.h>
+int main(){
+	int a,b;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	if(a>b){printf("%d is greater",a);}
+	else{printf("%d is greater",b);}
+	return 0;
+}
+	
