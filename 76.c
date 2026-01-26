97e9bba4080022b361b3c00f97c02489157d9e4b
Joosetha D <joosedhivya2006@gmail.com>
2026-01-26 17:33:27 +0530

    Add files via upload

---
+++ 76.c
@@ -0,0 +1,43 @@
+// 76.Vowel using switch
+#include <stdio.h>
+
+int main() {
+    char vowel;
+    scanf("%c",&vowel);
+    switch(vowel){
+        case 'a':
+            printf("Vowel");
+            break;
+        case 'e':
+            printf("Vowel");
+            break;
+        case 'i':
+            printf("Vowel");
+            break;
+        case 'o':
+            printf("Vowel");
+            break;
+        case 'u':
+            printf("Vowel");
+            break;
+        case 'A':
+            printf("Vowel");
+            break;
+        case 'E':
+            printf("Vowel");
+            break;
+        case 'I':
+            printf("Vowel");
+            break;
+        case 'O':
+            printf("Vowel");
+            break;
+        case 'U':
+            printf("Vowel");
+            break;
+        
+        default:
+            printf("Not a vowel");
+    }
+    return 0;
+}
