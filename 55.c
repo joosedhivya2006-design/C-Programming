890acc574d633dbcf5ea7f20adbc2719b9fdb0f2
Joosetha D <joosedhivya2006@gmail.com>
2026-01-26 08:00:20 +0530

    Add files via upload

---
+++ 55.c
@@ -0,0 +1,10 @@
+//55.Check Divisible by 5
+
+#include <stdio.h>
+int main(){
+	int number;
+	scanf("%d",&number);
+	if(number%5==0){printf("Divisible by 5");}
+	else{printf("Not Divisible by 5");}
+	return 0;
+}
