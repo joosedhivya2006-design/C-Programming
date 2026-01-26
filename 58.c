8a73274de883fc7379efb5691ad9bc347816ecef
Joosetha D <joosedhivya2006@gmail.com>
2026-01-26 08:01:07 +0530

    Add files via upload

---
+++ 58.c
@@ -0,0 +1,13 @@
+//58.Vowel or consonant
+
+//57.Check Leap Year
+
+#include <stdio.h>
+int main(){
+	char ch;
+	scanf("%c",&ch);
+	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+		printf("Vowel");}
+	else if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z')){printf("Consosnant");}
+	return 0;
+}
