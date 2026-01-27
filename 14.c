6293b227ec3e28279aebf1fff6e39011c634b430
Joosetha D <joosedhivya2006@gmail.com>
2026-01-27 21:56:33 +0530

    Add files via upload

--- 14.c
+++ 14.c
@@ -1,10 +1,28 @@
-//14.Add Two variables
-
-#include <stdio.h>
+#include<stdio.h>
 int main(){
-	int a,b;
-	scanf("%d",&a);
-	scanf("%d",&b);
-	printf("%d",a+b);
-	return 0;
+    int category,age;
+    scanf("%d %d",&category,&age);
+    if(age>=0 && age<=20){
+    switch (category)
+    {
+    case 1:
+        if(age <=5){
+            printf("$1500");
+        }
+        else if(age>5){
+            printf("$2500");
+        }
+        break;
+    case 2:
+        if(age<=5){
+            printf("$4000");
+        }
+        else if(age>5){
+            printf("$6000");
+        }
+    default:
+        printf("$4000");
+    }
+    }
+    return 0;
 }
