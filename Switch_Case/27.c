fa5600ba9dfafb3699da3da7100ab1a3784d1d20
Joosetha D <joosedhivya2006@gmail.com>
2026-01-27 22:04:35 +0530

    Add files via upload

---
+++ Switch_Case/27.c
@@ -0,0 +1,11 @@
+//27.Int to float
+
+#include <stdio.h>
+int main(){
+	int a;
+	scanf("%d",&a);
+	float value =(float)a;
+	printf("%.2f",value);
+	return 0;
+}
+
---
+++ Switch_Case/28.c
@@ -0,0 +1,9 @@
+//28.Store char in int
+
+#include <stdio.h>
+int main(){
+	char a;
+	scanf("%c",&a);
+	printf("%d",a);
+	return 0;
+}
