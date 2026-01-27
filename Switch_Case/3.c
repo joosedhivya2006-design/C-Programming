dd6eb7ef9e9827a119750f09e786240907d6298b
Joosetha D <joosedhivya2006@gmail.com>
2026-01-27 21:50:06 +0530

    Add files via upload

---
+++ Switch_Case/3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int type,balance,withdraw;
+    scanf("%d %d %d",&type,&balance,&withdraw);
+    switch(type){
+        case 1:
+            if(balance>=withdraw){
+                printf("Transaction Successful");
+            }
+            else if(withdraw>balance){
+                printf("Limit Exceded");
+            }
+            break;
+        case 2:
+            if(withdraw>0 && withdraw<=5000){
+                printf("Transaction Successfull");
+            }
+            else if(withdraw>5000){
+                printf("Limit Exceeded");
+            }
+            break;
+    }
+    return 0;
+}
