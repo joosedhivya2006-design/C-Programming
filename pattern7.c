986e52e354c331c9ff6a86c170f45f1cfb93a503
Joosetha D <joosedhivya2006@gmail.com>
2026-01-27 12:27:04 +0530

    Add files via upload

---
+++ pattern7.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n,space;
+    scanf("%d",&n);
+    for(int i=0;i<=n;i++){
+        
+        for(space=1;space<=n-i;space++){
+            printf(" ");
+        }
+        for(int j=1;j<=2*i-1;j++){
+            printf("*");
+        }
+        printf("\n");
+        
+    }
+
+    return 0;
+}
