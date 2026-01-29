a51e12227cc8608f5b00f3b00b29be135c773d55
Joosetha D <joosedhivya2006@gmail.com>
2026-01-29 09:06:21 +0530

    Add files via upload

---
+++ Patterns/pattern19.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<=n;i++){
+        
+        for(int j=0;j<=2*n+1;j++){
+        if((n-i<j) && (j<n+1+i)){
+            printf(" ");
+        }
+        else{
+            printf("*");
+        }}
+        printf("\n");  
+    }
+    for(int i=0;i<=n;i++){
+        
+        for(int j=0;j<=2*n+1;j++){
+        if(i<j && (j<2*n+1-i)){
+            printf(" ");
+        }
+        else{
+            printf("*");
+        }}
+        printf("\n");  
+    }
+    
+    return 0;
+}
