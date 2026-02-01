0ae09e558b150dfe890350da6c01484212e48d06
Joosetha D <joosedhivya2006@gmail.com>
2026-02-01 22:25:39 +0530

    Add files via upload

--- Arrays/1.c
+++ Arrays/1.c
@@ -1,1 +1,32 @@
-
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    if(n<2){
+        printf("Enter at least 2 elements");
+        return 0;
+    }
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int largest, secondLargest;
+    if (arr[0] > arr[1]) {
+        largest = arr[0];
+        secondLargest = arr[1];
+    } else {
+        largest = arr[1];
+        secondLargest = arr[0];
+    }
+    for (int i = 2; i < n; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] < largest && arr[i] > secondLargest) {
+            secondLargest = arr[i];
+        }
+    }
+    printf("%d", secondLargest);
+    return 0;
+}
