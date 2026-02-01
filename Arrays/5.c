0bed3d6198dfbebdb222fbb936b3dc5003cda085
Joosetha D <joosedhivya2006@gmail.com>
2026-02-01 22:27:00 +0530

    Add files via upload

---
+++ Arrays/5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n-1];
+    for(int i=0;i<n-1;i++){
+        scanf("%d",&arr[i]);
+    }
+    int sum_natural=n*(n+1)/2;
+    int sum=0;
+    for(int i=0;i<n-1;i++){
+        sum+=arr[i];
+    }
+    int missing=sum_natural-sum;
+    printf("%d",missing);
+    return 0;
+}
