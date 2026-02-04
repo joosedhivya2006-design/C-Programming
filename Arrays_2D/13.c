114636d220d502b7ed5b213afff1926d27c0dfa7
Joosetha D <joosedhivya2006@gmail.com>
2026-02-05 01:17:12 +0530

    Add files via upload

---
+++ Arrays_2D/13.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int row,column;
+    scanf("%d %d",&row,&column);
+    int arr[row][column];
+    for(int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int count=0;
+    int pallindrome=0;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            if(arr[i][j]==arr[i][column-1-j]){
+                pallindrome=1;
+            }
+            else{
+                pallindrome=0;
+            }
+        }
+        if(pallindrome){
+            count++;
+        }
+    }
+    printf("%d",count);
+    return 0;
+}
