6e3f406334f9e83ae3b4a5b840bd6732a473f977
Joosetha D <joosedhivya2006@gmail.com>
2026-02-05 11:54:52 +0530

    Add files via upload

---
+++ Arrays_2D/19.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int row,column;
+    scanf("%d %d",&row,&column);
+    int arr[row][column];
+    for(int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int count=0;
+    for(int i=0;i<row;i++){
+        int duplicate=0;
+        for (int j = 1; j < column; j++)
+        {
+            if(arr[i][j]==arr[i][j+i]){
+                duplicate=1;
+                break;
+            }        
+        }
+        if(!duplicate){
+            count++;
+        } 
+    }
+    printf("%d",count);
+    return 0;
+}
