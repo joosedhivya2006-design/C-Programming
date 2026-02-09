dd26784ff1425fb24a221a5fdb6ebd9bd999f711
Joosetha D <joosedhivya2006@gmail.com>
2026-02-09 09:45:35 +0530

    Create 4.Non_repeat.c

---
+++ String/4.Non_repeat.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    fgets(str,sizeof(str),stdin);
+    int start=0;
+    for(int i=0;str[i]!=0;i++){
+        if(str[i]==' '){
+            str[]
+        }
+    }
+    
+}
