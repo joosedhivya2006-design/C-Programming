b983d0f681ca5c44148dfadfe44152f7281da1c2
Joosetha D <joosedhivya2006@gmail.com>
2026-02-10 12:01:10 +0530

    Add files via upload

---
+++ String/8Space_count.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    fgets(str,sizeof(str),stdin);
+    int count=0;
+    for(int i=0;i<strlen(str);i++){
+        if(str[i]==' '){
+            count++;
+        }
+    }
+    printf("%d",count);
+    return 0;
+}
